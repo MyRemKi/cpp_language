c1f2fb524c580fcb559b4d97b3c329395f476642
MyRemKi <105482557+MyRemKi@users.noreply.github.com>
2023-08-27 14:57:28 +0200

    Add files via upload

---
+++ Morpion/Morpion/FramePrincipal.h
@@ -0,0 +1,66 @@
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version 3.9.0 Sep 14 2021)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO *NOT* EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <wx/artprov.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/button.h>
+#include <wx/string.h>
+#include <wx/bitmap.h>
+#include <wx/image.h>
+#include <wx/icon.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+#include <wx/sizer.h>
+#include <wx/textctrl.h>
+#include <wx/frame.h>
+
+///////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class FramePrincipal
+///////////////////////////////////////////////////////////////////////////////
+class FramePrincipal : public wxFrame
+{
+	private:
+
+	protected:
+		wxButton* m_button0;
+		wxButton* m_button1;
+		wxButton* m_button2;
+		wxButton* m_button3;
+		wxButton* m_button4;
+		wxButton* m_button5;
+		wxButton* m_button6;
+		wxButton* m_button7;
+		wxButton* m_button8;
+		wxTextCtrl* m_textCtrl1;
+
+		// Virtual event handlers, override them in your derived class
+		virtual void OnClickBC0( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnClickBC1( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnClickBC2( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnClickBC3( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnClickBC4( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnClickBC5( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnClickBC6( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnClickBC7( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnClickBC8( wxCommandEvent& event ) { event.Skip(); }
+
+
+	public:
+
+		FramePrincipal( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxT("CodeLite 14 wxFB 3.9 wxW 3.0"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 500,300 ), long style = wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL );
+
+		~FramePrincipal();
+
+};
+
---
+++ Morpion/Morpion/MyApp.h
@@ -0,0 +1,23 @@
+/***************************************************************
+ * Name:      MyApp.h
+ * Purpose:   Defines Application Class
+ * Author:    Éric Laly (elaly@free.fr)
+ * Created:   2007-09-21
+ * Copyright: Éric Laly ()
+ * License:
+ **************************************************************/
+
+#ifndef MYAPP_H
+#define MYAPP_H
+
+#include <wx/app.h>
+#include <wx/colour.h>
+class MyApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+    private:
+        wxLocale m_locale;
+};
+
+#endif // MYAPP_H
