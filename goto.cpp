b0475dac80abe99eb3f1897643fc77c95686838c
MyRemKi <105482557+MyRemKi@users.noreply.github.com>
2024-08-23 12:19:34 +0200

    Update goto.cpp

--- goto.cpp
+++ goto.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
 int main(){
-
+    int number=0;
+    
+    instructions:
+        number++;
+    
+    if(number<10){
+        goto instructions;
+    }
+    
+    return 0;
 }
