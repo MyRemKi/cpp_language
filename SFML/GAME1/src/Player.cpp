2a1ce2156dd4833ab5bf3417d932ecd30966af3e
MyRemKi <105482557+MyRemKi@users.noreply.github.com>
2024-11-05 17:11:35 +0100

    Update Player.cpp

--- SFML/GAME1/src/Player.cpp
+++ SFML/GAME1/src/Player.cpp
@@ -1,7 +1,21 @@
 #include "Player.h"
 
 Player::Player(){
+    BDD *playerbdd;
+    playerbdd=new BDD("PLAYER");
+    //Traiter les informations du Player
+}
 
+//Interaction
+bool Player::ReturnIsAlive(){
+    return (life_point>0);
+}
+
+int Player::SendDamage(){
+    return damage;
+}
+void Player::RecieveDamage(int r_damage){
+    life_point -= r_damage;
 }
 
 Player::~PLayer(){
