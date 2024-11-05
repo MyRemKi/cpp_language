38e5ef002ed992d5112d287f0876c136fece41e8
MyRemKi <105482557+MyRemKi@users.noreply.github.com>
2024-11-05 12:58:01 +0100

    Update Animation.h

--- SFML/GAME1/include/Animation.h
+++ SFML/GAME1/include/Animation.h
@@ -3,25 +3,9 @@
 
 class Animation {
 public:
-    std::vector<sf::IntRect> frames; // Rectangles de découpe des frames
-    sf::Sprite* sprite;              // Pointeur vers le sprite à animer
-    float frameTime;                 // Temps par frame
+    std::vector<sf::IntRect> frames;
+    sf::Sprite* sprite;
+    float frameTime;
     float elapsedTime;
     int currentFrame;
-
-    Animation(sf::Sprite* sprite, float frameTime) 
-        : sprite(sprite), frameTime(frameTime), elapsedTime(0), currentFrame(0) {}
-
-    void addFrame(const sf::IntRect& frame) {
-        frames.push_back(frame);
-    }
-
-    void update(float deltaTime) {
-        elapsedTime += deltaTime;
-        if (elapsedTime >= frameTime) {
-            elapsedTime = 0;
-            currentFrame = (currentFrame + 1) % frames.size();
-            sprite->setTextureRect(frames[currentFrame]);
-        }
-    }
 };
