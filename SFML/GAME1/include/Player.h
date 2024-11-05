e1c1f7e760165f5d5816bac47c775e7cef237a19
MyRemKi <105482557+MyRemKi@users.noreply.github.com>
2024-11-05 17:08:16 +0100

    Update Player.h

--- SFML/GAME1/include/Player.h
+++ SFML/GAME1/include/Player.h
@@ -1,1 +1,24 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 
+#include "BDD.H"
+
+class Player{
+
+    public:
+        Player();
+        ~Player();
+    
+        //Interaction
+        bool ReturnIsAlive();
+        int SendDamage();
+        void RecieveDamage(int r_damage);
+    private:
+        bool IsDead=false;
+        int life_point=0;
+        int damage=0;
+        int level=0;
+        int xp=0;
+        int next_max_xp;
+}
+#endif //PLAYER_H
