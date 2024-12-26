4e06b6de06adfb4b084727349d82ce90b053626c
MyRemKi <105482557+MyRemKi@users.noreply.github.com>
2024-12-26 10:30:30 +0100

    Update Clock.h

--- SFML/Clock.h
+++ SFML/Clock.h
@@ -1,13 +1,26 @@
 #ifndef CLOCKGAME_H
 #define CLOCKGAME_H
 
+#include <SFML/Graphics.hpp>
+
 Class ClockGame{
     public :
         ClassGame();
+        
+        bool GetShotClock();
+        void RestartShotClock();
+        
+        bool GetRecieveDamageClock();
+        void RestartRecievrDamageClock();
+
+        bool GetNextRoundClock();
+        void RestartNextRoundClock();
+        
         ~ClassGame();
 
     private :
-
+        sf::Clock recievedamageclock, nextroundclock, shotclock;
+        sf::Clock timer1, timer2, timer3;
 }
 
 #enfin CLOCKGAME_H
