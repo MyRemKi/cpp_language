7ae6dae1e2bb89be6607187e2f5bd885a9d48647
MyRemKi <105482557+MyRemKi@users.noreply.github.com>
2024-12-26 10:21:24 +0100

    Update Clock.h

--- SFML/Clock.h
+++ SFML/Clock.h
@@ -1,2 +1,13 @@
-#ifdef 
-#define
+#ifndef CLOCKGAME_H
+#define CLOCKGAME_H
+
+Class ClockGame{
+    public :
+        ClassGame();
+        ~ClassGame();
+
+    private :
+
+}
+
+#enfin CLOCKGAME_H
